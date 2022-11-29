b68f1cee678e947cf7f82ef51f63abf7d9e6dc68
qinfucheng <qinfc_al@163.com>
2022-11-29 19:48:39 +0800

    123

---
+++ Codeproject/project/build/CMakeFiles/h.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h.dir/main.cpp.obj"
+  "CMakeFiles/h.dir/main.cpp.obj.d"
+  "h.exe"
+  "h.exe.manifest"
+  "h.pdb"
+  "libh.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
